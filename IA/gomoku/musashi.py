a5e978c6be1661bb6bda08e0927f704d0d1aea7b
caian-gums <ile.caian@gmail.com>
2015-09-02 22:37:38 +0000

    Added the musashi class file.

---
+++ IA/gomoku/musashi.py
@@ -0,0 +1,13 @@
+# caian 02/09/2015
+"""
+Artificial Inteligence for the Gomoku game.
+"""
+
+class Musashi(object):
+    """
+    Musashi was a expert samurai who developed the techinique to fight with 2 swords. He was never defeated on combat.
+    The main intention to name this as Musashi is the 'double threat' concept to win the game. 2 swords, 'double threat'.
+    """
+
+    def __init__(self, board_size):
+        self.size = board_size
